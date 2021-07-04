7e112a54b22d9017a3f68ad7796f84886db6bf46
Marcin  Kowalczyk <kowalczyk.marcin@gmail.com>
2021-07-05 00:42:09 +0200

    animation for main background

--- src/GlobalStyled.js
+++ src/GlobalStyled.js
@@ -1,4 +1,12 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, keyframes } from 'styled-components';
+
+const BodyAnimation = keyframes`
+  0% { background: linear-gradient(to right, #eaeaeabe, #dbdbdbbc, #f2f2f2ba, #ada996c0); }
+  25% { background: linear-gradient(to right, #eaeaea8d, #dbdbdb89, #f2f2f284, #ada9967b); }
+  50% { background: linear-gradient(to right, #eaeaea57, #dbdbdb55, #f2f2f258, #ada99657); }
+  75% { background: linear-gradient(to right, #eaeaea26, #dbdbdb37, #f2f2f23b, #ada99644); }
+  100% { background: linear-gradient(to right, #eaeaea, #dbdbdb, #f2f2f2, #ada996); }
+`;
 
 const GlobalStyle = createGlobalStyle`
 * {
@@ -16,7 +24,7 @@ body {
     #f2f2f2,
     #ada996
   );
-    background: linear-gradient(to right, #eaeaea, #dbdbdb, #f2f2f2, #ada996);
+  animation: ${BodyAnimation} 24s linear infinite;
   }
 `;
 
